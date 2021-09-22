6881c31918e5dc8d0faaf324129e406aff1f0a06
dankos <daniil.koryto@gmail.com>
2021-09-23 06:08:26 +1000

    Initial commit

---
+++ src/test/java/ru/dankos/moneyloverwalletservice/MoneyLoverWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dankos.moneyloverwalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLoverWalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
