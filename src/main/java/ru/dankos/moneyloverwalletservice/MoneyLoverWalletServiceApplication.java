e93fb734a24c1fee7df23fd7f362fc5c8ee12672
dankos <daniil.koryto@gmail.com>
2021-10-08 10:55:12 +1000

    added eureka client annotation

--- src/main/java/ru/dankos/moneyloverwalletservice/MoneyLoverWalletServiceApplication.java
+++ src/main/java/ru/dankos/moneyloverwalletservice/MoneyLoverWalletServiceApplication.java
@@ -2,8 +2,10 @@ package ru.dankos.moneyloverwalletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoneyLoverWalletServiceApplication {
 
     public static void main(String[] args) {
