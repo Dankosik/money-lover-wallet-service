ba8cc616c2f4f663acadedc19cd52e20b0d2e8d0
dankos <daniil.koryto@gmail.com>
2021-10-08 10:52:07 +1000

    added RabbitConfig

---
+++ src/main/java/ru/dankos/moneyloverwalletservice/config/RabbitConfig.java
@@ -0,0 +1,30 @@
+package ru.dankos.moneyloverwalletservice.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitMQHost;
+
+    @Value("${spring.rabbitmq.port}")
+    private Integer rabbitMQPort;
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(rabbitMQHost, rabbitMQPort);
+    }
+}
